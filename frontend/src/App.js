810350599512cd0187b921050e55074dfc372145
Uzaif-Sheikh <uzaifsheikh2309@gmail.com>
2022-02-26 17:30:53 +1100

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, Link, TextField, Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AppBar position='fixed' style={{backgroundColor:'#FDA0A0',paddingLeft:'1%',paddingRight:'1%'}}>
+      <Toolbar>
+      <Link style={{margin:'5px',cursor:'pointer',color:'white'}}>About</Link>
+      <Link style={{margin:'5px',cursor:'pointer',color:'white'}}>Log Out</Link>
+      </Toolbar>
+    </AppBar>
+    <div style={{height:'650px',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',marginTop:'60px'}}>
+      <TextField style={{margin:'5px'}} helperText="Please enter your email" id="demo-helper-text-misaligned" label="Email"/>
+      <TextField style={{margin:'5px'}} helperText="Please enter your password" id="demo-helper-text-misaligned" label="Password" type='password'/>
+      <div style={{margin:'5px'}}>
+        <Button style={{backgroundColor:'#F06868',margin:'10px'}}>Log in</Button>
+        <Button style={{backgroundColor:'#F06868',margin:'10px'}}>Sign up</Button>
+      </div>
+    </div>
     </div>
   );
 }
